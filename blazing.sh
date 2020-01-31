2e7107ecc0c501223f0b49ce3a1ead9dc6600f7f
pandavpn03 <55373447+pandavpn03@users.noreply.github.com>
2020-01-31 13:58:29 +0800

    Update blazing.sh

--- blazing.sh
+++ blazing.sh
@@ -3,3 +3,9 @@ wget -O /etc/openvpn/script/config.sh "https://raw.githubusercontent.com/pandavp
 cd /etc/openvpn/script/
 chmod +x config.sh
 service openvpn restart
+rm blazing.sh
+history -c
+echo "Server is already authenticated to BlazingFast Account of Linor VPN"
+echo "Please click enter to exit"
+exit
+
